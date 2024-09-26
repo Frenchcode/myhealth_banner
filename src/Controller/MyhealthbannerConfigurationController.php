eba0689bf03d8f2bed2e4d134cb7ce68592e8869
Ephraim Bokuma Mulenga <ephraim.bokuma-mulenga.epitech.eu>
2024-09-26 16:55:37 +0200

    [ADD] Added module route

---
+++ src/Controller/MyhealthbannerConfigurationController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+
+namespace PrestaShop\Module\MyHealthBanner\Controller;
+
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MyhealthbannerConfigurationController extends FrameworkBundleAdminController
+{
+    public function index(Request $request): Response
+    {
+        $textFormDataHandler = $this->get('prestashop.module.myhealthbanner.form.myhealthbanner_configuration_text_form_data_handler');
+
+        $textForm = $textFormDataHandler->getForm();
+        $textForm->handleRequest($request);
+
+        if ($textForm->isSubmitted() && $textForm->isValid()) {
+            $errors = $textFormDataHandler->save($textForm->getData());
+
+            if (empty($errors)) {
+                $this->addFlash('success', $this->trans('Successfull updated.', 'Admin.Notifications.Success'));
+                return $this->redirectToRoute('myhealthbanner_configuration');
+            }
+            $this->flashErrors($errors);
+        }
+        return $this->render('@Modules/MyHealthBanner/Resources/views/templates/admin/form.html.twig', [
+            'myhealthbannerConfiguration' => $textForm->createView()
+        ]);
+    }
+}
